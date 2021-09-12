54e801f907ee2074c47daa2a034dfc2c8bbdeb26
Sean Zheng <seanzhengw@gmail.com>
2021-09-12 23:08:15 +0800

    feat: sockaddr_in6 only

--- src/SockAddr.cpp
+++ src/SockAddr.cpp
@@ -3,35 +3,30 @@
 #include <arpa/inet.h>
 #include <memory.h>
 
-SockAddr::SockAddr() : data(nullptr), len(0) {}
+SockAddr::SockAddr()
+    : data(new char[sizeof(sockaddr_in6)]), len(sizeof(sockaddr_in6)) {
+    memset(data, 0, sizeof(sockaddr_in6));
+}
 
-SockAddr::SockAddr(const char *ip, int port) {
-    struct in6_addr in6;
-    if (inet_pton(AF_INET6, ip, &in6) == 1) {
-        sockaddr_in6 *sa = new sockaddr_in6;
-        memset(sa, 0, sizeof(sockaddr_in6));
-        sa->sin6_family = AF_INET6;
-        sa->sin6_addr = in6;
-        sa->sin6_port = htons(port);
-        this->data = sa;
-        this->len = sizeof(sockaddr_in6);
-        return;
+SockAddr::SockAddr(const char *ip, int port)
+    : data(new char[sizeof(sockaddr_in6)]), len(sizeof(sockaddr_in6)) {
+    memset(data, 0, sizeof(sockaddr_in6));
+    sockaddr_in6 *sa = (sockaddr_in6 *)data;
+
+    bool ok = inet_pton(AF_INET6, ip, &(sa->sin6_addr)) == 1;
+    if (!ok) {
+        if (strlen(ip) <= INET_ADDRSTRLEN) {
+            char ipstr[INET6_ADDRSTRLEN];
+            sprintf(ipstr, "::FFFF:%s", ip);
+            ok = inet_pton(AF_INET6, ipstr, &(sa->sin6_addr)) == 1;
+        }
+        if (!ok) {
+            return;
+        }
     }
 
-    struct in_addr in;
-    if (inet_pton(AF_INET, ip, &in) == 1) {
-        sockaddr_in *sa = new sockaddr_in;
-        memset(sa, 0, sizeof(sockaddr_in));
-        sa->sin_family = AF_INET;
-        sa->sin_addr = in;
-        sa->sin_port = htons(port);
-        this->data = sa;
-        this->len = sizeof(sockaddr_in);
-        return;
-    }
-
-    this->data = nullptr;
-    this->len = 0;
+    sa->sin6_family = AF_INET6;
+    sa->sin6_port = htons(port);
 }
 
 SockAddr::SockAddr(const SockAddr &sa) {
@@ -48,25 +43,10 @@ SockAddr &SockAddr::operator=(const SockAddr &sa) {
 }
 
 std::string SockAddr::str() const {
-    if (data == nullptr) {
-        return std::string();
-    }
-
+    sockaddr_in6 *addr = (sockaddr_in6 *)data;
     char ipstr[INET6_ADDRSTRLEN];
-    in_port_t port;
-    switch (((sockaddr *)data)->sa_family) {
-    case AF_INET: {
-        sockaddr_in &sa = *(sockaddr_in *)data;
-        inet_ntop(AF_INET, &(sa.sin_addr), ipstr, INET6_ADDRSTRLEN);
-        port = ntohs(sa.sin_port);
-    } break;
-    case AF_INET6: {
-        sockaddr_in6 &sa6 = *(sockaddr_in6 *)data;
-        inet_ntop(AF_INET6, &(sa6.sin6_addr), ipstr, INET6_ADDRSTRLEN);
-        port = ntohs(sa6.sin6_port);
-    } break;
-    }
-    return std::string(ipstr) + ":" + std::to_string(port);
+    inet_ntop(AF_INET6, &(addr->sin6_addr), ipstr, INET6_ADDRSTRLEN);
+    return std::string(ipstr) + ":" + std::to_string(ntohs(addr->sin6_port));
 }
 
 void SockAddr::set(void *data, unsigned int len) {
